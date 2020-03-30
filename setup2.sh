24006e6f573e4f2c9551162b59b3aaf9e6678501
Brad Stoner <bstoner@gmail.com>
2020-03-30 13:09:52 -0700

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,3 @@
+yum -y install *.rpm
+yum -y install java-11-openjdk-headless-11.0.2.7 java-11-openjdk-11.0.2.7 java-11-openjdk-devel-11.0.2.7
+yum -y install moogsoft-db moogsoft-common moogsoft-integrations moogsoft-integrations-ui moogsoft-mooms moogsoft-search moogsoft-server moogsoft-ui moogsoft-ccsm moogsoft-utils
